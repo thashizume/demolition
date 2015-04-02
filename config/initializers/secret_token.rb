b6b7185a8aaaeb18c10d6a08de7df8e6d538cf38
Toshinobu Hashizume <toshinobu.hashizume@gmail.com>
2015-04-02 23:06:19 +0900

    first Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demolition::Application.config.secret_key_base = '2338a39abe9c9a7a2e41cd3a721b25d34aa4fb7b0b3e563f66e9dd36312ba0ae5ec77c5ec53973e793848b374231d8a49c29a9697595f0ba68a5b8287fb22b0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Demolition::Application.config.session_store :cookie_store, key: '_demolition_session'
